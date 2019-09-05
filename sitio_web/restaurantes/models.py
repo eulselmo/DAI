ec19e618864f48d2e3adfa01866414964545d3b5
Ángel García Illescas <tessio1994@gmail.com>
2019-09-06 00:20:19 +0200

    Add files via upload

---
+++ sitio_web/restaurantes/models.py
@@ -0,0 +1,6 @@
+from pymongo import MongoClient
+
+# Create your models here.
+client = MongoClient()
+db = client.test                  # base de datos
+restaurantes = db.restaurants     # colección
